e2eec8393fd82af89a437734cf538ea4f5f54698
andreisizyi <60555925+andreisizyi@users.noreply.github.com>
2020-02-17 12:55:19 +0200

    Update menu-wp.php

--- menu-wp.php
+++ menu-wp.php
@@ -1,12 +1,11 @@
 <?php
 
-if( $menu_items = wp_get_nav_menu_items('menu') ) { // "Меню для шапки" - это название моего меню. Вы можете также использовать ID или ярлык
+if( $menu_items = wp_get_nav_menu_items('menu') ) {
 	$menu_list = '';
 	$this_item = get_queried_object_id();
 	foreach ( (array) $menu_items as $key => $menu_item ) {
-		$title = $menu_item->title; // заголовок элемента меню (анкор ссылки)
-		$url = $menu_item->url; // URL ссылки
-		$id = $menu_item->object_id; // URL ссылки
+		$title = $menu_item->title;
+		$url = $menu_item->url;
 		$activeclass = '';
 		
 		if($id == $this_item){
